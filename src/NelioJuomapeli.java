bb0075afe1713cbc62ceab88d76207cc94a8b7ca
Aleksi Eskola <apesko@utu.fi>
2018-02-21 20:14:31 +0200

    Toimiva peli ilman rivien poistamista

--- src/NelioJuomapeli.java
+++ src/NelioJuomapeli.java
@@ -1,43 +1,32 @@
+import java.util.ArrayList;
+
 public class NelioJuomapeli{
 
     public static void main(String[] args) {
         Pelipoyta p = new Pelipoyta();
-        p.luoPelipoyta();
-        /*for (int i=1; i<=52; i++) {
-            System.out.print("" + i + ". " + p.annaKortit().get(i-1).annaMaa() + " ");
-            System.out.println(p.annaKortit().get(i-1).annaArvo());
-        }*/
 
-        //tulostetaan pelipöytä
-
-        for (int i=0; i<=8; i+=1) {
-            if (i==2 || i==5 || i==8) {
-                System.out.print("|  " + p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaArvo());
-                System.out.print(p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaMaa() + " ");
-                System.out.println(p.annaPoyta().get(i).size() >= 10 ? "(" + p.annaPoyta().get(i).size() + ") |" : "(0" + p.annaPoyta().get(i).size() + ") |" );
+        while (p.p.annaKortit().size() > 0) {
+            for (int i = 0; i <= 8; i += 1) {
+                if (i == 2 || i == 5 || i == 8) {
+                    System.out.print("|  " + p.annaPoyta().get(i).get(p.annaPoyta().get(i).size() - 1).annaArvo());
+                    System.out.print(p.annaPoyta().get(i).get(p.annaPoyta().get(i).size() - 1).annaMaa() + " ");
+                    System.out.println(p.annaPoyta().get(i).size() >= 10 ? "(" + p.annaPoyta().get(i).size() + ") |" : "(0" + p.annaPoyta().get(i).size() + ") |");
+                } else {
+                    System.out.print("|  " + p.annaPoyta().get(i).get(p.annaPoyta().get(i).size() - 1).annaArvo());
+                    System.out.print(p.annaPoyta().get(i).get(p.annaPoyta().get(i).size() - 1).annaMaa() + " ");
+                    System.out.print(p.annaPoyta().get(i).size() >= 10 ? "(" + p.annaPoyta().get(i).size() + ") " : "(0" + p.annaPoyta().get(i).size() + ") ");
+                }
             }
-            else {
-                System.out.print("|  " + p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaArvo());
-                System.out.print(p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaMaa() + " ");
-                System.out.print(p.annaPoyta().get(i).size() >= 10 ? "(" + p.annaPoyta().get(i).size() + ") " :  "(0" + p.annaPoyta().get(i).size() + ") " );
+            System.out.println("Kortteja jäljellä: " + p.p.annaKortit().size());
+
+            if (p.p.annaKortit().size() > 0) {
+                p.pelaaKortti();
             }
         }
-        p.pelaaKortti();
-        for (int i=0; i<=8; i+=1) {
-            if (i==2 || i==5 || i==8) {
-                System.out.print("|  " + p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaArvo());
-                System.out.print(p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaMaa() + " ");
-                System.out.println(p.annaPoyta().get(i).size() >= 10 ? "(" + p.annaPoyta().get(i).size() + ") |" : "(0" + p.annaPoyta().get(i).size() + ") |" );
-            }
-            else {
-                System.out.print("|  " + p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaArvo());
-                System.out.print(p.annaPoyta().get(i).get(p.annaPoyta().get(i).size()-1).annaMaa() + " ");
-                System.out.print(p.annaPoyta().get(i).size() >= 10 ? "(" + p.annaPoyta().get(i).size() + ") " :  "(0" + p.annaPoyta().get(i).size() + ") " );
-            }
-        }
-        /* System.out.println();
-        p.uusiKortti(4);
-        System.out.print(p.annaPoyta().get(4).get(p.annaPoyta().get(4).size()-1).annaMaa() + " ");
-        System.out.println(p.annaPoyta().get(4).get(p.annaPoyta().get(4).size()-1).annaArvo()); */
+
+
+
+        System.out.println("Kurkku kävi oksentaa!");
+        System.exit(0);
     }
 }
--- src/Pelipoyta.java
+++ src/Pelipoyta.java
@@ -1,37 +1,44 @@
 import java.util.*;
 
 public class Pelipoyta extends Korttipakka{
-
     ArrayList<ArrayList<Kortti>> poyta;
     Korttipakka p;
+    int oikeat = 0;
 
-    public Pelipoyta() {
+    public ArrayList<ArrayList<Kortti>> annaPoyta(){
+        return poyta;
+    }
+
+    public Korttipakka annaKorttipakka(){
+        return p;
+    }
+
+    public Pelipoyta(){
         luoPelipoyta();
     }
 
     public void luoPelipoyta() {
         poyta = new ArrayList<ArrayList<Kortti>>();
-        this.p = new Korttipakka();
-        p.luoUusiPakka();
+        p = new Korttipakka();
         for (int i =0; i<9; i++){
             poyta.add(new ArrayList<Kortti>());
         }
         for (int i=0; i<9; i++) {
-            poyta.get(i).add(p.annaKortit().get(0));
-            p.annaKortit().remove(0);
+            Kortti jako = p.annaKortit().get(0);
+            poyta.get(i).add(jako);
+            p.kortit.remove(0);
         }
     }
 
     public void uusiKortti(int i) {
         poyta.get(i).add(p.annaKortit().get(0));
+        p.poistaKortti();
     }
 
     public void pelaaKortti() {
-        int oikeat = 0;
-
         Scanner sc = new Scanner(System.in);
-
         int pino = 0;
+
         while (!(pino >= 1 && pino <= 9)) {
             System.out.print("Valitse pelattava pino (1-9): ");
             pino = sc.nextInt();
@@ -41,40 +48,42 @@ public class Pelipoyta extends Korttipakka{
         while (merkki != '+' && merkki != '-') {
             System.out.print("Onko seuraava kortti suurempi + vai pienempi - ?: ");
             merkki = sc.next().charAt(0);
+            System.out.println("");
         }
-
-        sc.close();
 
         while (true) {
             if (merkki == '+') {
                 if (p.annaKortit().get(0).compareTo(poyta.get(pino-1).get(poyta.get(pino-1).size()-1)) > 0) {
                     oikeat++;
                     System.out.println("Oikein!");
+                    if (oikeat==3){
+                        System.out.println("Seuraavan pelaajan vuoro");
+                        oikeat=0;
+                    }
                     break;
                 }
                 else {
-                    System.out.println("Väärin");
+                    System.out.println("Väärin! Juo " + (poyta.get(pino-1).size()+1));
                     break;
                 }
             } else if (merkki == '-') {
                 if (p.annaKortit().get(0).compareTo(poyta.get(pino-1).get(poyta.get(pino-1).size()-1)) < 0) {
                     oikeat++;
                     System.out.println("Oikein!");
+                    if (oikeat==3){
+                        System.out.println("Seuraavan pelaajan vuoro");
+                        oikeat=0;
+                    }
                     break;
                 }
                 else {
-                    System.out.println("Väärin");
+                    System.out.println("Väärin! Juo " + (poyta.get(pino-1).size()+1));
                     break;
                 }
             }
         }
+        System.out.println("Oikeita vastauksia: " + oikeat);
 
         this.uusiKortti(pino-1);
     }
-
-    //GET
-    public ArrayList<ArrayList<Kortti>> annaPoyta() {
-        return this.poyta;
-    }
-
 }
